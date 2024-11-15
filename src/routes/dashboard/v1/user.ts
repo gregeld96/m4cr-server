f42707d0e77559125924e62ab13c8566322c2c44
Gregorius Eldwin Pradipta <gregeld96@gmail.com>
2024-11-15 17:16:29 +0700

    Add missing validator

--- src/routes/dashboard/v1/user.ts
+++ src/routes/dashboard/v1/user.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
+import { validateData } from "src/middlewares/zod_validation";
 import UserAdminController from "src/modules/user/admin.controller";
+import { createInternalAccountSchema } from "src/validator/user";
 
 const UserRoutes = Router();
 
 // UserRoutes.get('/status/:category', MasterSettingController.statusBasedCategory);
-UserRoutes.post('/', UserAdminController.create);
+UserRoutes.post('/', validateData(createInternalAccountSchema), UserAdminController.create);
 // UserRoutes.put('/:id', MasterSettingController.getRoles);
 // UserRoutes.delete('/soft-delete/:id', MasterSettingController.getRoles);
 
