cc13278882e11f4382913234e3fcac7d1fad159c
jessicaverna <jessicavernaaa@gmail.com>
2024-12-07 14:49:04 +0700

    feat: add endpoint gemini

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,17 +2,17 @@ import { Router } from 'express';
 import ApiVerRoutes from './api';
 import DashboardVerRoutes from './dashboard';
 import SocialRoutes from './social';
+import Carbon from './carbon';
 
 const routes = Router();
 
-routes.use(
-	'/api', 
-	ApiVerRoutes
-);
+routes.use('/api', ApiVerRoutes);
 routes.use(
 	'/dashboard',
 	DashboardVerRoutes
 );
+
+routes.use('/carbon', Carbon);
 
 routes.use('/social', SocialRoutes);
 
